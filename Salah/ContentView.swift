919e7af624dfe19da82da440cd685da418123311
MahiAlJawad <br.mahialjawad@gmail.com>
2024-02-18 13:28:17 +0600

    Initial Commit

---
+++ Salah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Salah
+//
+//  Created by Mahi Al Jawad on 18/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Salah/SalahApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalahApp.swift
+//  Salah
+//
+//  Created by Mahi Al Jawad on 18/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SalahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
